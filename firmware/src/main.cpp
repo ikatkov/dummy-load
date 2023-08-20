4cf9e53bf28b55e55a12b6a661879f6c3bde7bab
ikatkov <ikatkov@gmail.com>
2023-08-20 14:27:38 -0700

    lcd display

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,23 +1,25 @@
 #include "Arduino.h"
-#include <TM1637Display.h>
+#include <LiquidCrystal_I2C.h>
 #define CLK 4
 #define DIO_V 2
 #define DIO_I 3
 #define EXT_V_REF 4.55
 
-TM1637Display displayV(CLK, DIO_V);
-TM1637Display displayI(CLK, DIO_I);
-uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
-uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
+LiquidCrystal_I2C lcd(0x3f, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
     Serial.begin(9600);
 
-    displayV.setBrightness(75);
-    displayI.setBrightness(255);
-    displayV.setSegments(data);
-    displayI.setSegments(data);
+    lcd.init(); // initialize the lcd
+    // Print a message to the LCD.
+    lcd.backlight();
+    // lcd.setCursor(3, 0);
+    // lcd.print("Hello, world!");
+    // lcd.setCursor(2, 1);
+    // lcd.print("Ywrobot Arduino!");
+    // lcd.setCursor(0, 2);
+    // lcd.print("Arduino LCM IIC 2004");
 
     analogReference(EXTERNAL); // use AREF for reference voltage
 }
@@ -33,8 +35,14 @@ void loop()
     float voltage = (sensorValue + 0.5) * EXT_V_REF / 1024.0;
     float temp = voltage * 25.21025227 - 2.20439897;
     Serial.println(temp);
+    lcd.setCursor(0, 0);
+    lcd.print("12V");
+    lcd.setCursor(0, 1);
+    lcd.print("1.25A");
+    lcd.setCursor(11, 0);
+    lcd.print(temp);
+    lcd.setCursor(14, 0);
+    lcd.print(" C");
 
-    displayI.showNumberDecEx(temp, 0, false);
-    displayV.showNumberDecEx(counter, 0b10000000, true);
     delay(1000);
 }
